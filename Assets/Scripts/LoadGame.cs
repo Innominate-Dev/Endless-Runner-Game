3e25d62a8b0f8e3c3027a8e98d2231431a3472b6
Innominate-Dev <InnominateDev@gmail.com>
2022-10-04 12:43:19 +0100

    Menu Update
    New MENU and logic buttons

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Will load the next build scene whichw e set by going into build and set a hieracrcy for scenes e.g main Menu is lvl 0 which means its the first thing the player sees.
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit(); 
+    }
+}
